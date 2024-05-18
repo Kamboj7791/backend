d576463fe58200a87274b73e8c72c2c362641200
kamboj7791 <kambojkunal01@gmail.com>
2024-05-19 03:19:04 +0530

    demo application is done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -10,6 +10,12 @@ app.get("/kunal", (req, res) => {
 });
 app.get("/kamboj", (req, res) => {
   res.send("you entered kamboj route");
+});
+app.get("/login", (req, res) => {
+  res.send(`<h1>please enter details here</h1>`);
+});
+app.get("/follow", (req, res) => {
+  res.send("<h2>please follow my github account for more source code</h2>");
 });
 
 app.listen(port, () => {
