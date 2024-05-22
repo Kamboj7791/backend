ce309f78737d2ed6846bc38e766018368363a943
kamboj7791 <kambojkunal01@gmail.com>
2024-05-23 01:37:45 +0530

    full-stack-app

---
+++ full-stack-app/backend/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+app.get("/", (req, res) => {
+  res.send("SERVER IS READY");
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
+});
