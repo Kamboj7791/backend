a0a4f981e8a74943d7a14a7b5222a06d66dd5e21
kamboj7791 <kambojkunal01@gmail.com>
2024-05-23 23:43:23 +0530

    data added

--- full-stack-app/backend/server.js
+++ full-stack-app/backend/server.js
@@ -1,7 +1,28 @@
 import express from "express";
 const app = express();
+const data = [
+  {
+    id: 1,
+    name: "kunal",
+  },
+  {
+    id: 2,
+    name: "kamboj",
+  },
+  {
+    id: 3,
+    name: "mukesh",
+  },
+  {
+    id: 4,
+    name: "kumar",
+  },
+];
 app.get("/", (req, res) => {
   res.send("SERVER IS READY");
+});
+app.get("/data", (req, res) => {
+  res.send(data);
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
